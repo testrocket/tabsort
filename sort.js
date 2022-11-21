0702b7d658d85374e5fc138c5cb07cf95f734214
testrocket <heychristmastree@gmail.com>
2022-11-21 18:01:45 +0700

    test - update formatting

--- sort.js
+++ sort.js
@@ -1,49 +1,53 @@
-var lastURL = ""
-chrome.tabs.onUpdated.addListener(function(id, changeInfo, tab) {
-	if (changeInfo.status === "complete" && lastURL !== parse(tab.url)) {
-		moveTabs()
-		lastURL = parse(tab.url);
-	}
+var lastURL = "";
+chrome.tabs.onUpdated.addListener(function (id, changeInfo, tab) {
+  if (changeInfo.status === "complete" && lastURL !== parse(tab.url)) {
+    moveTabs();
+    lastURL = parse(tab.url);
+  }
 });
 
 function moveTabs() {
-	chrome.tabs.query({}, function(tabs) {
-		var urls = [];
-		for (var i = 0; i < tabs.length; i++) {
-			urls.push(tabs[i].url);	
-		}
-		
-		urls.sort(function(url1, url2) {
-			return parse(url1).localeCompare(parse(url2));
-		});
-		
-		var tabData = [];
-		for (var i = 0; i < urls.length; i++) {
-			for (var j = 0; j < tabs.length; j++) {
-				if (tabs[j].url === urls[i]) {
-					tabData.push({id: tabs[j].id, index: i });
-				}	
-			}
-		}
-		
-		for (var i = 0; i < tabData.length; i++) {
-			chrome.tabs.move(tabData[i].id, { index: tabData[i].index }, function(tabs) {});
-		}
-	});
+  chrome.tabs.query({}, function (tabs) {
+    var urls = [];
+    for (var i = 0; i < tabs.length; i++) {
+      urls.push(tabs[i].url);
+    }
+
+    urls.sort(function (url1, url2) {
+      return parse(url1).localeCompare(parse(url2));
+    });
+
+    var tabData = [];
+    for (var i = 0; i < urls.length; i++) {
+      for (var j = 0; j < tabs.length; j++) {
+        if (tabs[j].url === urls[i]) {
+          tabData.push({ id: tabs[j].id, index: i });
+        }
+      }
+    }
+
+    for (var i = 0; i < tabData.length; i++) {
+      chrome.tabs.move(
+        tabData[i].id,
+        { index: tabData[i].index },
+        function (tabs) {}
+      );
+    }
+  });
 }
 
 function parse(url) {
-	var fixed = url.substring(url.indexOf('://') + 3);
-	var index = fixed.indexOf('/');
-	
-	index = index === -1 ? fixed.length : index;
-	fixed = fixed.substring(0, index);
-	
-	index = fixed.lastIndexOf('.');
-	if (index === -1) {
-		return fixed;
-	}
-	
-	index = fixed.lastIndexOf('.', index - 1);
-	return index === -1 ? fixed : fixed.substring(index + 1);
+  var fixed = url.substring(url.indexOf("://") + 3);
+  var index = fixed.indexOf("/");
+
+  index = index === -1 ? fixed.length : index;
+  fixed = fixed.substring(0, index);
+
+  index = fixed.lastIndexOf(".");
+  if (index === -1) {
+    return fixed;
+  }
+
+  index = fixed.lastIndexOf(".", index - 1);
+  return index === -1 ? fixed : fixed.substring(index + 1);
 }
